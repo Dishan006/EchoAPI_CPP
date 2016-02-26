04ae777e0f029e83b1b23ccb8c3c224db7ce1a64
Dishan Metihakwala <dishansm@gmail.com>
2016-02-26 16:11:30 +0100

    Add files

---
+++ src/Main.cpp
@@ -0,0 +1,49 @@
+//============================================================================
+// Name        : Main.cpp
+// Author      : Dishan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+	return 0;
+}
+
+extern "C" const char* ProcessRequest(char* request, char* method, char* body, char**headers, int hederCount) {
+
+	ostringstream hederCountString;
+	hederCountString << hederCount;
+
+	std::string* requestString = new std::string();
+	requestString->append("{ \"request\" : \"");
+	requestString->append(request);
+	requestString->append("\" , \"method\" :\"");
+	requestString->append(method);
+	requestString->append("\" , \"hederCount\" :");
+	requestString->append(hederCountString.str());
+	requestString->append(" ,\n\"headers\" :[");
+
+	for(int i=0;i<hederCount;i++)
+	{
+		if(i!=0)
+		{
+			requestString->append(" , ");
+		}
+		requestString->append("\"");
+		requestString->append(headers[i]);
+		requestString->append("\"");
+	}
+
+
+	requestString->append("]\n , \"body\" :");
+	requestString->append(body);
+	requestString->append(" }");
+
+	return requestString->c_str();
+}
